cdca1c08185c2502458079d4a947f9fac69d02a9
whatshow <wfwfpwefe2323@gmail.com>
2024-05-31 11:28:57 +1000

    demos use new locale

--- src/pages/demos/demo01.js
+++ src/pages/demos/demo01.js
@@ -23,9 +23,9 @@ export class Demo01 extends React.Component{
                 <Content className='page-demo-01-background'>
                     <div className="page-demo-01-container" style={{ minHeight: 380 }}>
                         <div className='Title'>
-                            <FormattedMessage id="main_blogger_title"/>
+                            <FormattedMessage id="demo_01_blogger_title"/>
                             <div className='Subtitle'>
-                                <FormattedMessage id="main_blogger_subtitle"/>
+                                <FormattedMessage id="demo_01_blogger_subtitle"/>
                             </div>
                         </div>
                         <Carousel autoplay className='fuck'>
@@ -47,64 +47,64 @@ export class Demo01 extends React.Component{
                         </Carousel>
                         {/* <iframe src="https://ourworldindata.org/grapher/total-cases-covid-19?tab=map" width="100%" height="600px"></iframe> */}
                         <div className='Title'>
-                            <FormattedMessage id="main_destination_title"/>
+                            <FormattedMessage id="demo_01_destination_title"/>
                             <div className='Subtitle'>
-                                <FormattedMessage id="main_destination_subtitle"/>
+                                <FormattedMessage id="demo_01_destination_subtitle"/>
                             </div>
                         </div>
                         <ul className='Destinations'>
                             <li>
-                            <div><FormattedMessage id="main_destination_ex_1"/></div>
+                            <div><FormattedMessage id="demo_01_destination_ex_1"/></div>
                             </li>
                             <li>
-                                <div><FormattedMessage id="main_destination_ex_2"/></div>
+                                <div><FormattedMessage id="demo_01_destination_ex_2"/></div>
                             </li>
                             <li>
-                                <div><FormattedMessage id="main_destination_ex_3"/></div>
+                                <div><FormattedMessage id="demo_01_destination_ex_3"/></div>
                             </li>
                             <li>
-                            <   div><FormattedMessage id="main_destination_ex_4"/></div>
+                            <   div><FormattedMessage id="demo_01_destination_ex_4"/></div>
                             </li>
                         </ul>
                         <div className='Title'>
-                            <FormattedMessage id="main_service_title"/>
+                            <FormattedMessage id="demo_01_service_title"/>
                             <div className='Subtitle'>
-                                <FormattedMessage id="main_service_subtitle"/>
+                                <FormattedMessage id="demo_01_service_subtitle"/>
                             </div>
                         </div>
                         <div className='service-container'>
                             <ul>
                                 <li>
                                     <p><SnippetsTwoTone/></p>
-                                    <p><FormattedMessage id="main_service_1_name"/></p>
-                                    <p><FormattedMessage id="main_service_1_intro"/></p>
+                                    <p><FormattedMessage id="demo_01_service_1_name"/></p>
+                                    <p><FormattedMessage id="demo_01_service_1_intro"/></p>
                                 </li>
                                 <li>
                                     <p><WarningTwoTone/></p>
-                                    <p><FormattedMessage id="main_service_2_name"/></p>
-                                    <p><FormattedMessage id="main_service_2_intro"/></p>
+                                    <p><FormattedMessage id="demo_01_service_2_name"/></p>
+                                    <p><FormattedMessage id="demo_01_service_2_intro"/></p>
                                 </li>
                                 <li>
                                     <p><ApiTwoTone/></p>
-                                    <p><FormattedMessage id="main_service_3_name"/></p>
-                                    <p><FormattedMessage id="main_service_3_intro"/></p>
+                                    <p><FormattedMessage id="demo_01_service_3_name"/></p>
+                                    <p><FormattedMessage id="demo_01_service_3_intro"/></p>
                                 </li>
                             </ul>
                             <ul>
                                 <li>
                                     <p><EditTwoTone/></p>
-                                    <p><FormattedMessage id="main_service_4_name"/></p>
-                                    <p><FormattedMessage id="main_service_4_intro"/></p>
+                                    <p><FormattedMessage id="demo_01_service_4_name"/></p>
+                                    <p><FormattedMessage id="demo_01_service_4_intro"/></p>
                                 </li>
                                 <li>
                                     <p><MailTwoTone /></p>
-                                    <p><FormattedMessage id="main_service_5_name"/></p>
-                                    <p><FormattedMessage id="main_service_5_intro"/></p>
+                                    <p><FormattedMessage id="demo_01_service_5_name"/></p>
+                                    <p><FormattedMessage id="demo_01_service_5_intro"/></p>
                                 </li>
                                 <li>
                                     <p><LockTwoTone /></p>
-                                    <p><FormattedMessage id="main_service_6_name"/></p>
-                                    <p><FormattedMessage id="main_service_6_intro"/></p>
+                                    <p><FormattedMessage id="demo_01_service_6_name"/></p>
+                                    <p><FormattedMessage id="demo_01_service_6_intro"/></p>
                                 </li>
                             </ul>
                         </div>
